35ed7a55d9f5f9fa1aa062ed6ffe42bb6aee6660
Noah Moroze <moroze@mit.edu>
2017-05-29 22:00:57 +0000

    Fixed bugs in refactored cm1k

--- cm1k.py
+++ cm1k.py
@@ -1,48 +1,48 @@
 import smbus
 
+I2C_ADDRESS = 0x4A
+
+# registers
+REG_NSR = 0x0D
+REG_GCR = 0x0B
+REG_MINIF = 0x06
+REG_MAXIF = 0x07
+REG_NCR = 0x00
+REG_COMP = 0x01
+REG_LCOMP = 0x02
+REG_INDEXCOMP = 0x03
+REG_DIST = 0x03
+REG_CAT = 0x04
+REG_AIF = 0x05
+REG_NID = 0x0A
+REG_POWERSAVE = 0x0E
+REG_FORGET = 0x0F
+REG_NCOUNT = 0x0F
+REG_RESETCHAIN = 0x0C
+
+# test registers
+REG_TESTCOMP = 0x08
+REG_TESTCAT = 0x09
+
 class CM1K(object):
-    I2C_ADDRESS = 0x4A
-
-    # registers
-    REG_NSR = 0x0D
-    REG_GCR = 0x0B
-    REG_MINIF = 0x06
-    REG_MAXIF = 0x07
-    REG_NCR = 0x00
-    REG_COMP = 0x01
-    REG_LCOMP = 0x02
-    REG_INDEXCOMP = 0x03
-    REG_DIST = 0x03
-    REG_CAT = 0x04
-    REG_AIF = 0x05
-    REG_NID = 0x0A
-    REG_POWERSAVE = 0x0E
-    REG_FORGET = 0x0F
-    REG_NCOUNT = 0x0F
-    REG_RESETCHAIN = 0x0C
-
-    # test registers
-    REG_TESTCOMP = 0x08
-    REG_TESTCAT = 0x09
-
-    def __init__(bus=1):
+    def __init__(self, bus=1):
         self.bus = smbus.SMBus(bus)
 
-    def read(reg):
-        word = bus.read_word_data(I2C_ADDRESS, reg)
+    def read(self, reg):
+        word = self.bus.read_word_data(I2C_ADDRESS, reg)
         return ((word>>8)&0xff) + ((word&0xff)<<8) 
 
-    def write(reg, value=0):
-        bus.write_word_data(I2C_ADDRESS, reg, ((value>>8)&0xff)+((value&0xff)<<8))
+    def write(self, reg, value=0):
+        self.bus.write_word_data(I2C_ADDRESS, reg, ((value>>8)&0xff)+((value&0xff)<<8))
 
-    def read_component():
+    def read_component(self):
         data = []
         for _ in xrange(256):
             data.append(self.read(REG_COMP))
         self.write(REG_INDEXCOMP, 0)
         return data
 
-    def train_vector(vector, category):
+    def train_vector(self, vector, category):
         for byte in vector[:-1]: 
             self.write(REG_COMP, byte)  
         
--- test.py
+++ test.py
@@ -4,17 +4,17 @@ import unittest
 
 class TestCM1K(unittest.TestCase):
     def test_category(self):
-        cm1k = cm1k.CM1K()
+        cm = cm1k.CM1K()
         random.random()
         category = random.randint(1, 0x7FFE)
 
-        cm1k.write(REG_NSR, 0x10)
-        cm1k.write(REG_CAT, category)
+        cm.write(cm1k.REG_NSR, 0x10)
+        cm.write(cm1k.REG_TESTCAT, category)
 
-        cm1k.write(REG_RESETCHAIN)
+        cm.write(cm1k.REG_RESETCHAIN)
         n_count = 0
         while True:
-            cat = cm1k.read(REG_CAT)
+            cat = cm.read(cm1k.REG_CAT)
             if cat == 0xFFFF:
                 self.assertEqual(n_count, 1024)
                 break
